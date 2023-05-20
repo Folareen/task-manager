e61fd4fb444bad5e2092e22c161a673fbfe83b48
Folareen <sakawahab03@gmail.com>
2023-05-20 19:02:53 +0100

    setup

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -6,7 +6,22 @@ const createTask = (req, res) => {
   res.send("Create Task");
 };
 
+const getTask = (req, res) => {
+  res.send("Get Task");
+};
+
+const updateTask = (req, res) => {
+  res.send("Update Task");
+};
+
+const deleteTask = (req, res) => {
+  res.send("Delete Task");
+};
+
 module.exports = {
   getAllTasks,
   createTask,
+  getTask,
+  updateTask,
+  deleteTask
 };
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,7 +1,8 @@
 const { Router } = require("express");
 const tasksRouter = Router();
-const { getAllTasks, createTask } = require("../controllers/tasks");
+const { getAllTasks, createTask, getTask, updateTask, deleteTask } = require("../controllers/tasks");
 
 tasksRouter.route("/").get(getAllTasks).post(createTask);
+tasksRouter.route("/:id").get(getTask).patch(updateTask).delete(deleteTask);
 
 module.exports = tasksRouter;
