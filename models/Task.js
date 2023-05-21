a8674b7b4d99346129597a0b5e0d47d8aec8372b
Folareen <sakawahab03@gmail.com>
2023-05-21 08:44:01 +0100

    done with task schema

--- models/Task.js
+++ models/Task.js
@@ -2,8 +2,16 @@ const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
 const TaskSchema = new Schema({
-  name: String,
-  completed: Boolean,
+  name: {
+    type: String,
+    required: [true, "Name cannot be empty"],
+    trim: true,
+    maxlength: [20, "Name cannot be more than 20 characters"],
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 const Task = model("Task", TaskSchema);
