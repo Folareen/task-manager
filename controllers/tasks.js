f7ef84eab2a24a3b70f5f614cffe4df3d2b27d42
Folareen <sakawahab03@gmail.com>
2023-05-21 12:36:42 +0100

    done with 'update task'

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -26,7 +26,7 @@ const createTask = async (req, res) => {
 
 const getTask = async (req, res) => {
   try {
-    const task = await Task.find({_id : req.params.id})
+    const task = await Task.find({ _id: req.params.id })
 
     if (!task) return res.status(404).json({ message: "Task does not exist." });
 
@@ -36,8 +36,18 @@ const getTask = async (req, res) => {
   }
 };
 
-const updateTask = (req, res) => {
-  res.send("Update Task");
+const updateTask = async (req, res) => {
+  try {
+    if(!req.body.name) return res.status(400).json({ message: "Name is required!" })
+    
+    const task = await Task.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true, runValidators: true })
+
+    if (!task) return res.status(404).json({ message: "Task does not exist." });
+
+    res.status(200).json({ task });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
 
 const deleteTask = (req, res) => {
